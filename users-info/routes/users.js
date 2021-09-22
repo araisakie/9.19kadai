8837547a9555e31fbd43a5b4209df7e9c22b4cec
新井沙紀枝 <araisakie@araisakiedanoMacBook-Air.local>
2021-09-22 13:41:48 +0900

    削除とidに紐付いた情報取得のAPIを作成

--- users-info/routes/users.js
+++ users-info/routes/users.js
@@ -3,21 +3,68 @@ const router = express.Router();
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database("users.db");
 
-router.get("/", (req, res, next) => {
+/* ホーム画面読み込み */
+router.get("/api/v1", (req, res) => {
   try {
     db.serialize(() => {
-      db.all("select * from users", (err, rows) => {
-        if (!err) {
-          const data = {
-            title: "Users Info",
-            content: rows,
-          };
-          res.render("users", data);
+      db.all(
+        "select id, name, age, email, telephone from users",
+        (err, rows) => {
+          res.status(200).json({ userData: rows });
         }
-      });
+      );
     });
-  } catch (error) {
-    console.log(error);
+  } catch (e) {
+    res.status(500).json({
+      errorMessage: "データーベースのアクセスに失敗しました",
+      error: e,
+    });
   }
 });
+
+/* idに紐づいたAPI(取得) */
+router.get("/api/v1/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    db.serialize(() => {
+      db.get(`select * from users where id = "${id}"`, (err, row) =>
+        res.status(200).json({ Database: row })
+      );
+    });
+  } catch (e) {
+    res.status(500).json({ errorMessage: "取得に失敗しました", error: e });
+  }
+});
+
+/* 新規登録 */
+router.post("/api/v1", (req, res) => {
+  try {
+    const { name, email, age, telephone } = req.body;
+    db.serialize(() => {
+      db.post(
+        `insert into users (name, email, age, telephone) values("${name}","${email}","${age}","${telephone}")`,
+        () => res.status(200).json({ message: "ユーザーの登録に成功しました" })
+      );
+    });
+  } catch (e) {
+    res
+      .status(500)
+      .json({ errorMessage: "ユーザーの登録に失敗しました", error: e });
+  }
+});
+
+/* 削除 */
+router.delete("/api/v1/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    db.serialize(() => {
+      db.delete(`delete from users where id = "${id}"`, () =>
+        res.status(200).json({ message: "削除できました" })
+      );
+    });
+  } catch (e) {
+    res.status(500).json({ errorMessage: "削除できませんでした", error: e });
+  }
+});
+
 module.exports = router;
